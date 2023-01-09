b0c7423bbfd2e28f11d165af24cdfdcf736e1622
Michael Kreil <github@michael-kreil.de>
2023-01-09 22:33:15 +0100

    --store only once

--- bin/processing-scripts/3_convert.sh
+++ bin/processing-scripts/3_convert.sh
@@ -7,7 +7,7 @@ aria2c -o "$TILE_NAME.osm.pbf" --seed-time=0 "$TILE_SRC"
 
 if [ ${#TILE_BBOX} -ge 1 ]
 then
-	../tilemaker --bbox $TILE_BBOX --input "$TILE_NAME.osm.pbf" --store tilemaker-cache.dat --config config.json --process process.lua --output "$TILE_NAME.mbtiles" --store ./tmp --compact
+	../tilemaker --bbox $TILE_BBOX --input "$TILE_NAME.osm.pbf" --config config.json --process process.lua --output "$TILE_NAME.mbtiles" --store ./tmp --compact
 else
-	../tilemaker --input "$TILE_NAME.osm.pbf" --store tilemaker-cache.dat --config config.json --process process.lua --output "$TILE_NAME.mbtiles" --store ./tmp --compact
+	../tilemaker --input "$TILE_NAME.osm.pbf" --config config.json --process process.lua --output "$TILE_NAME.mbtiles" --store ./tmp --compact
 fi
