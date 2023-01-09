d4b66ba2aea8aaff037a97fe82c60d90eb96fcf3
Michael Kreil <github@michael-kreil.de>
2023-01-09 22:26:18 +0100

    fix

--- bin/processing-scripts/3_convert.sh
+++ bin/processing-scripts/3_convert.sh
@@ -5,7 +5,7 @@ set -ex
 
 aria2c -o "$TILE_NAME.osm.pbf" --seed-time=0 "$TILE_SRC"
 
-if [ $TILE_BBOX -ge 1 ]
+if [ "$TILE_BBOX" -ge 1 ]
 then
 	../tilemaker --bbox $TILE_BBOX --input "$TILE_NAME.osm.pbf" --store tilemaker-cache.dat --config config.json --process process.lua --output "$TILE_NAME.mbtiles" --store ./tmp --compact
 else
