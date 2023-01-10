7c09bd1b02ece9b037d7dcf79779461c948553db
Michael Kreil <github@michael-kreil.de>
2023-01-10 10:29:39 +0100

    move scripts

--- bin/google_compute_vm/2_generate_tiles.sh
+++ bin/google_compute_vm/2_generate_tiles.sh
@@ -37,8 +37,45 @@ gcloud compute instances create opencloudtiles-generator \
 	--metadata \
 	TILE_SRC="$tile_src",TILE_BBOX="$tile_bbox",TILE_NAME="$tile_name",TILE_DST="$tile_dst"
 
-sleep 10
+sleep 15
 
 gcloud compute ssh opencloudtiles-generator --zone europe-west3-c --command='curl -Ls "https://github.com/OpenCloudTiles/opencloudtiles-generator/raw/main/bin/startup-scripts/run_google_compute_vm.sh" | bash'
 
 gcloud compute instances delete opencloudtiles-generator --zone europe-west3-c
+
+# Todo:
+# dont create and destroy machine, use images
+
+
+
+#!/bin/bash
+
+set -ex
+
+script_src="https://github.com/OpenCloudTiles/opencloudtiles-generator/raw/main/bin"
+env_file="/etc/profile"
+
+function get_env() {
+	key=$1
+	value=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/$key" -H "Metadata-Flavor: Google")
+	cmd="export $key=\"$value\""
+	eval "$cmd"
+	echo "$cmd" | sudo tee -a $env_file
+}
+
+get_env "TILE_SRC"
+get_env "TILE_BBOX"
+get_env "TILE_NAME"
+get_env "TILE_DST"
+
+source $env_file
+
+curl -Ls "$script_src/processing-scripts/1_setup.sh" | sudo bash
+curl -Ls "$script_src/processing-scripts/2_prepare_tilemaker.sh" | bash
+curl -Ls "$script_src/processing-scripts/3_convert.sh" | bash
+
+cd ~/tilemaker/build/shortbread-tilemaker
+
+gsutil cp "$TILE_NAME.mbtiles" "$TILE_DST"
+
+sudo shutdown -P now
