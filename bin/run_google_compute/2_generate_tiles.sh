d68ce17fb9c4682dffd32dc9080260e6887c4367
Michael Kreil <github@michael-kreil.de>
2023-01-10 13:21:05 +0100

    test planet

--- bin/run_google_compute/2_generate_tiles.sh
+++ bin/run_google_compute/2_generate_tiles.sh
@@ -15,11 +15,11 @@ set -e
 # Prepare env variables
 tile_bbox=""
 tile_dst="gs://opencloudtiles/mbtiles/"
-tile_name="eu-de-bw"; tile_src="https://download.geofabrik.de/europe/germany/baden-wuerttemberg-latest.osm.pbf"; machine_type="n2d-highcpu-8"
-# tile_name="eu-de-be"; tile_src="https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf"; machine_type="n2d-highcpu-8"
-# tile_name="eu-de"; tile_src="https://download.geofabrik.de/europe/germany-latest.osm.pbf"; machine_type="n2d-standard-16"
+# tile_name="eu-de-be"; tile_src="https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf"; machine_type="n2d-highcpu-8"   # 1:22 = 0.01 $
+# tile_name="eu-de-bw"; tile_src="https://download.geofabrik.de/europe/germany/baden-wuerttemberg-latest.osm.pbf"; machine_type="n2d-highcpu-8"   # 3:05 = 0.02 $
+# tile_name="eu-de"; tile_src="https://download.geofabrik.de/europe/germany-latest.osm.pbf"; machine_type="n2d-standard-16"   # 11:13 = 0.14 $
 # tile_name="eu"; tile_src="https://download.geofabrik.de/europe-latest.osm.pbf"; machine_type="n2d-highmem-32"
-# tile_name="planet"; tile_src="https://planet.osm.org/pbf/planet-latest.osm.pbf.torrent"; machine_type="n2d-highmem-64"
+tile_name="planet"; tile_src="https://planet.osm.org/pbf/planet-latest.osm.pbf.torrent"; machine_type="n2d-highmem-64"
 
 # create VM from image
 gcloud compute instances create opencloudtiles-generator \
@@ -36,13 +36,11 @@ do
 done
 
 # prepare command and run it via SSH
-command="export TILE_SRC=\"$tile_src\";export TILE_BBOX=\"$tile_bbox\";export TILE_NAME=\"$tile_name\";"
+command="export TILE_SRC=\"$tile_src\";export TILE_BBOX=\"$tile_bbox\";export TILE_NAME=\"$tile_name\""
 command="$command; curl -Ls \"https://github.com/OpenCloudTiles/opencloudtiles-generator/raw/main/bin/basic_scripts/3_convert.sh\" | bash"
-command="$command; gsutil cp \"~/tilemaker/build/shortbread-tilemaker/data/$tile_name.mbtiles\" \"$tile_dst\""
+command="$command; gsutil cp \"tilemaker/build/shortbread-tilemaker/data/$tile_name.mbtiles\" \"$tile_dst\""
 command="$command; sudo shutdown -P now"
 
-echo "$command"
-
-gcloud compute ssh opencloudtiles-generator --command="$command"
+gcloud compute ssh opencloudtiles-generator --command="$command" --quiet
 
 gcloud compute instances delete opencloudtiles-generator --quiet
