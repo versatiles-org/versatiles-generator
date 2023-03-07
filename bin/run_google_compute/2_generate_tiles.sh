8f17a489983417ae1d2a560a92114f48ed40636d
Michael Kreil <github@michael-kreil.de>
2023-03-07 09:29:15 +0100

    fix regexp

--- bin/run_google_compute/2_generate_tiles.sh
+++ bin/run_google_compute/2_generate_tiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+set -e
+
 # machine_type="n2d-highmem-2";  # 16 GB RAM
 # machine_type="n2d-highmem-4";  # 32 GB RAM
 # machine_type="n2d-highmem-8";  # 64 GB RAM
@@ -17,31 +19,31 @@ COLUMNS=1
 select source in "Berlin" "Baden-Württemberg" "Germany" "Europe" "Planet"; do
 	case $source in
 		"Berlin") # 0:01:22 = 0.01$
-			src_date=$(curl -s "https://download.geofabrik.de/europe/germany/berlin.html" | egrep -o 'href="berlin-(\d{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			src_date=$(curl -s "https://download.geofabrik.de/europe/germany/berlin.html" | egrep -o 'href="berlin-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
 			tile_src="https://download.geofabrik.de/europe/germany/berlin-$src_date.osm.pbf"
 			tile_dst="gs://versatiles/download/planet/europe/germany/berlin-$src_date.mbtiles"
 			machine_type="n2d-highcpu-8"
 			break;;
 		"Baden-Württemberg") # 0:03:05 = 0.02 $
-			src_date=$(curl -s "https://download.geofabrik.de/europe/germany/baden-wuerttemberg.html" | egrep -o 'href="baden-wuerttemberg-(\d{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			src_date=$(curl -s "https://download.geofabrik.de/europe/germany/baden-wuerttemberg.html" | egrep -o 'href="baden-wuerttemberg-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
 			tile_src="https://download.geofabrik.de/europe/germany/baden-wuerttemberg-$src_date.osm.pbf"
 			tile_dst="gs://versatiles/download/planet/europe/germany/bw-$src_date.mbtiles"
 			machine_type="n2d-highcpu-8"
 			break;;
 		"Germany") # 0:11:13 = 0.14 $
-			src_date=$(curl -s "https://download.geofabrik.de/europe/germany.html" | egrep -o 'href="germany-(\d{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			src_date=$(curl -s "https://download.geofabrik.de/europe/germany.html" | egrep -o 'href="germany-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
 			tile_src="https://download.geofabrik.de/europe/germany-$src_date.osm.pbf"
 			tile_dst="gs://versatiles/download/planet/europe/germany/germany-$src_date.mbtiles"
 			machine_type="n2d-standard-16"
 			break;;
 		"Europe")
-			src_date=$(curl -s "https://download.geofabrik.de/europe.html" | egrep -o 'href="europe-(\d{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			src_date=$(curl -s "https://download.geofabrik.de/europe.html" | egrep -o 'href="europe-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
 			tile_src="https://download.geofabrik.de/europe-$src_date.osm.pbf"
 			tile_dst="gs://versatiles/download/planet/europe/europe-$src_date.mbtiles"
 			machine_type="n2d-highmem-32"
 			break;;
 		"Planet") # 11:18:20 = 42.50 $
-			src_date=$(curl -s "https://planet.osm.org/pbf/" | egrep -o 'href="planet-(\d{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			src_date=$(curl -s "https://planet.osm.org/pbf/" | egrep -o 'href="planet-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
 			tile_src="https://planet.osm.org/pbf/planet-$src_date.osm.pbf.torrent"
 			tile_dst="gs://versatiles/download/planet/planet-$src_date.mbtiles"
 			machine_type="n2d-highmem-64"
