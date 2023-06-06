659caca1c2fad921be2739e9435526c5d06c3cb7
Michael Kreil <github@michael-kreil.de>
2023-06-06 08:40:59 +0200

    8 digit date

--- bin/run_google_compute/2_generate_tiles.sh
+++ bin/run_google_compute/2_generate_tiles.sh
@@ -19,33 +19,33 @@ COLUMNS=1
 select source in "Berlin" "Baden-Württemberg" "Germany" "Europe" "Planet"; do
 	case $source in
 		"Berlin") # 0:01:22 = 0.01$
-			src_date=$(curl -s "https://download.geofabrik.de/europe/germany/berlin.html" | egrep -o 'href="berlin-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
-			tile_src="https://download.geofabrik.de/europe/germany/berlin-$src_date.osm.pbf"
-			tile_dst="gs://versatiles/download/planet/europe/germany/berlin-$src_date.mbtiles"
+			date=$(curl -s "https://download.geofabrik.de/europe/germany/berlin.html" | egrep -o 'href="berlin-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			tile_src="https://download.geofabrik.de/europe/germany/berlin-$date.osm.pbf"
+			tile_dst="gs://versatiles/download/planet/europe/germany/berlin-20$date.mbtiles"
 			machine_type="n2d-highcpu-8"
 			break;;
 		"Baden-Württemberg") # 0:03:05 = 0.02 $
-			src_date=$(curl -s "https://download.geofabrik.de/europe/germany/baden-wuerttemberg.html" | egrep -o 'href="baden-wuerttemberg-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
-			tile_src="https://download.geofabrik.de/europe/germany/baden-wuerttemberg-$src_date.osm.pbf"
-			tile_dst="gs://versatiles/download/planet/europe/germany/bw-$src_date.mbtiles"
+			date=$(curl -s "https://download.geofabrik.de/europe/germany/baden-wuerttemberg.html" | egrep -o 'href="baden-wuerttemberg-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			tile_src="https://download.geofabrik.de/europe/germany/baden-wuerttemberg-$date.osm.pbf"
+			tile_dst="gs://versatiles/download/planet/europe/germany/bw-20$date.mbtiles"
 			machine_type="n2d-highcpu-8"
 			break;;
 		"Germany") # 0:11:13 = 0.14 $
-			src_date=$(curl -s "https://download.geofabrik.de/europe/germany.html" | egrep -o 'href="germany-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
-			tile_src="https://download.geofabrik.de/europe/germany-$src_date.osm.pbf"
-			tile_dst="gs://versatiles/download/planet/europe/germany/germany-$src_date.mbtiles"
+			date=$(curl -s "https://download.geofabrik.de/europe/germany.html" | egrep -o 'href="germany-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			tile_src="https://download.geofabrik.de/europe/germany-$date.osm.pbf"
+			tile_dst="gs://versatiles/download/planet/europe/germany/germany-20$date.mbtiles"
 			machine_type="n2d-standard-16"
 			break;;
 		"Europe")
-			src_date=$(curl -s "https://download.geofabrik.de/europe.html" | egrep -o 'href="europe-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
-			tile_src="https://download.geofabrik.de/europe-$src_date.osm.pbf"
-			tile_dst="gs://versatiles/download/planet/europe/europe-$src_date.mbtiles"
+			date=$(curl -s "https://download.geofabrik.de/europe.html" | egrep -o 'href="europe-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			tile_src="https://download.geofabrik.de/europe-$date.osm.pbf"
+			tile_dst="gs://versatiles/download/planet/europe/europe-20$date.mbtiles"
 			machine_type="n2d-highmem-32"
 			break;;
 		"Planet") # 11:18:20 = 42.50 $
-			src_date=20$(curl -s "https://planet.osm.org/pbf/" | egrep -o 'href="planet-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
-			tile_src="https://planet.osm.org/pbf/planet-$src_date.osm.pbf.torrent"
-			tile_dst="gs://versatiles/download/planet/planet-$src_date.mbtiles"
+			date=20$(curl -s "https://planet.osm.org/pbf/" | egrep -o 'href="planet-([0-9]{6}).osm.pbf"' | sed -n 's/.*-\([0-9]\{6\}\)\..*/\1/p' | sort | tail -n1)
+			tile_src="https://planet.osm.org/pbf/planet-$date.osm.pbf.torrent"
+			tile_dst="gs://versatiles/download/planet/planet-20$date.mbtiles"
 			machine_type="n2d-highmem-64"
 			tile_bbox="-180,-90,180,90"
 			break;;
